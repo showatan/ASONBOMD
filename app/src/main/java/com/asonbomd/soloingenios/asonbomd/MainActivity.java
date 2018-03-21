4ae9b5d7d9ef33261ef2d2620c391367b0d1e979
Edgar Chavez <edgarchavez4@gmail.com>
2018-03-21 13:01:23 -0600

    Se puede pedir permisos y tomar la fotografia

--- app/src/main/java/com/asonbomd/soloingenios/asonbomd/MainActivity.java
+++ app/src/main/java/com/asonbomd/soloingenios/asonbomd/MainActivity.java
@@ -1,18 +1,28 @@
 package com.asonbomd.soloingenios.asonbomd;
 
+import android.Manifest;
 import android.content.ContentResolver;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.media.ExifInterface;
 import android.media.MediaScannerConnection;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -26,10 +36,15 @@ import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+import static android.Manifest.permission_group.CAMERA;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -41,6 +56,9 @@ public class MainActivity extends AppCompatActivity
     String path;
     private final String CARPETA_RAIZ="imgprueba/";
     private final String RUTA_IMAGEN=CARPETA_RAIZ+"MisFotos";
+    private static final int REQUEST_CODE_ASK_PERMISSIONS = 123;
+    private static final int REQUEST_CODE_ASK_PERMISSIONSAL = 123;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,25 +74,20 @@ public class MainActivity extends AppCompatActivity
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-       /* NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-        Spinner spinner1 = (Spinner) findViewById(R.id.sp_tipoem);
-        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.datos, R.layout.support_simple_spinner_dropdown_item);
-        spinner1.setAdapter(adapter); parte para el manejo de spinner*/
 
        //Codigo del imagenview en donde se captura la foto de la emergencia
+
         boto1=(ImageButton)findViewById(R.id.captura);
         boto1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                capturar();
+               checkPermissioncam();
 
 
             }
         });
        //se termina el codido del boton
-        //Intent ListSong = new Intent(getApplicationContext(),Directorio.class);
-        //startActivity(ListSong);
+
     }
 
     @Override
@@ -120,75 +133,120 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
-    private void capturar() {
-       /* timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        foto = Environment.getExternalStorageDirectory() + "/"+timeStamp+".jpg";
-        file=new File(foto);
-        Intent in =new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        output = Uri.fromFile(file);
-        in.putExtra(MediaStore.EXTRA_OUTPUT, output);
-        in.putExtra(MediaStore.EXTRA_SIZE_LIMIT,102*20);
-        startActivityForResult(in, 1);*/
 
-       File fileImagen = new File(Environment.getExternalStorageDirectory(),RUTA_IMAGEN);
-       boolean iscreada = fileImagen.exists();
-       if (iscreada==false) {
-           iscreada=fileImagen.mkdirs();
-       }
-        if (iscreada==false) {
-            timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+
+
+
+    ////////////////////////////////
+
+
+    private void checkPermissioncam() {
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+
+            Toast.makeText(this, "This version is not Android 6 or later " + Build.VERSION.SDK_INT, Toast.LENGTH_LONG).show();
+
+        } else {
+
+            int hasWriteContactsPermission = checkSelfPermission(Manifest.permission.CAMERA);
+            int almacenamiento = checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            int ubicacion = checkCallingPermission(Manifest.permission.ACCESS_FINE_LOCATION);
+
+
+            if ((hasWriteContactsPermission != PackageManager.PERMISSION_GRANTED)||
+                    (almacenamiento != PackageManager.PERMISSION_GRANTED)||
+                    (ubicacion != PackageManager.PERMISSION_GRANTED)) {
+
+                requestPermissions(new String[] {Manifest.permission.CAMERA,WRITE_EXTERNAL_STORAGE,ACCESS_FINE_LOCATION},
+                        REQUEST_CODE_ASK_PERMISSIONS);
+
+                Toast.makeText(this, "Requesting permissions", Toast.LENGTH_LONG).show();
+
+            }else if ((hasWriteContactsPermission == PackageManager.PERMISSION_GRANTED)&&
+                    (almacenamiento == PackageManager.PERMISSION_GRANTED)&&
+                    (ubicacion == PackageManager.PERMISSION_GRANTED)){
+
+                Toast.makeText(this, "The permissions are already granted ", Toast.LENGTH_LONG).show();
+                capturar();
+
+            }
 
         }
+
+        return;
+    }
+    private void checkPermissionalm(){
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if(REQUEST_CODE_ASK_PERMISSIONS == requestCode) {
+            if ((grantResults[0] == PackageManager.PERMISSION_GRANTED)&&
+                    (grantResults[1] == PackageManager.PERMISSION_GRANTED)
+                    &&(grantResults[2] == PackageManager.PERMISSION_GRANTED)) {
+                Toast.makeText(this, "OK Permissions granted ! :-) " + Build.VERSION.SDK_INT, Toast.LENGTH_LONG).show();
+                capturar();
+            } else {
+                Toast.makeText(this, "Permissions are not granted ! :-( " + Build.VERSION.SDK_INT, Toast.LENGTH_LONG).show();
+            }
+        }else{
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+    ///////////////////
+
+
+
+
+
+    private void capturar() {
+
+
+       File fileImagen = new File(Environment.getExternalStorageDirectory(),RUTA_IMAGEN);
+
+        timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         path=Environment.getExternalStorageDirectory()+File.separator+RUTA_IMAGEN+File.separator+timeStamp+".jpg";
         File imagen = new File(path);
 
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(imagen));
-        startActivityForResult(intent, 20);
+
+        Intent intent = null;
+        intent= new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.N)
+        {
+            String authorities=getApplicationContext().getPackageName()+".provider";
+            Uri imageUri= FileProvider.getUriForFile(this,authorities,imagen);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        }else
+        {
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(imagen));
+        }
+        startActivityForResult(intent,20);
+
+
+        //intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(imagen));
+        //startActivityForResult(intent, 20);
 
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-       /*ContentResolver cr = this.getContentResolver();
-        //imag =(ImageView) findViewById(R.id.imageView2);
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inSampleSize = 5;
-        Bitmap bm = BitmapFactory.decodeFile(foto, options);
-        Bitmap bit = null;
-        try {
-            bit = bm;
-            int rotate = 0;
-            ExifInterface exif = new ExifInterface((file.getAbsolutePath()));
-            int orientation = exif.getAttributeInt(
-                    ExifInterface.TAG_ORIENTATION,
-                    ExifInterface.ORIENTATION_NORMAL);
-            switch (orientation) {
-                case ExifInterface.ORIENTATION_ROTATE_270:
-                    rotate = 270;
-                    break;
-                case ExifInterface.ORIENTATION_ROTATE_180:
-                    rotate = 180;
-                    break;
-                case ExifInterface.ORIENTATION_ROTATE_90:
-                    rotate = 90;
-                    break;
-            }
-
-            Matrix matrix = new Matrix();
-            matrix.postRotate(rotate);
-            matrix.preScale(150, 150);
-            bit = Bitmap.createBitmap(bm, 0, 0, bit.getWidth(),bit.getHeight(), matrix, true);
 
 
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
 
-        //BitmapFactory.Options options = new BitmapFactory.Options();
-        //options.inSampleSize = 2; Bitmap bm = BitmapFactory.decodeFile(foto, options);
-        boto1.setImageBitmap(bit);*/
        if (resultCode==RESULT_OK){
            MediaScannerConnection.scanFile(this, new String[]{path}, null,
                    new MediaScannerConnection.OnScanCompletedListener() {
@@ -197,12 +255,15 @@ public class MainActivity extends AppCompatActivity
                            Log.i("Ruta de Almacenamineto","path: "+path);
                        }
                    });
-           Bitmap bitmap = BitmapFactory.decodeFile(path);
+           BitmapFactory.Options options = new BitmapFactory.Options();
+           options.inSampleSize = 5;
+           Bitmap bitmap = BitmapFactory.decodeFile(path, options);
            boto1.setImageBitmap(bitmap);
 
 
        }
 
-        }
+
+    }
 
 }
